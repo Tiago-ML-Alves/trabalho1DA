3ac2e08aee2ca6e8a00e7268f989aaf5f58fd3b6
dapinto-alx <up202404733@up.pt>
2026-03-18 21:52:00 +0000

    types.h fully implemented

---
+++ Project1SampleDatasets/src/types.h
@@ -0,0 +1,64 @@
+#ifndef PROJECT1_TYPES_H
+#define PROJECT1_TYPES_H
+
+#include <iostream>
+#include <map>
+
+enum class VertexType;
+
+struct Paper
+{
+    int id;
+    std::string title;
+    std::string authorName;
+    std::string email;
+    int primaryTopic;
+    int secondaryTopic;
+};
+
+struct Reviewer
+{
+    int id;
+    std::string name;
+    std::string email;
+    int primaryTopic;
+    int secondaryTopic;
+};
+
+struct Parameters
+{
+    int maxReviewsPerReviewer;
+    int minReviewsPerSubmission;
+    int primaryReviewExpertise;
+    int secondaryReviewerExpertise;
+    int primarySubmissionDomain;
+    int secondarySubmissionDomain;
+};
+
+struct Control
+{
+    int generateAssignments;
+    int riskAnalysis;
+    std::string outputFileName = "output.csv";
+};
+
+struct NodeInfo
+{
+    int id;
+    VertexType type;
+
+    bool operator==(const NodeInfo& other) const
+    {
+        return id == other.id && type == other.type;
+    }
+};
+enum class VertexType
+{
+    SOURCE,
+    PAPER,
+    REVIEWER,
+    SINK
+};
+
+
+#endif
